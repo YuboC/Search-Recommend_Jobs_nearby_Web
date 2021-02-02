c7f6ea8113cd3cf93d0a9313b851697edfb56452
Yubo <lalagola010@gmail.com>
2021-02-02 10:36:43 -0500

    first commit

---
+++ src/main/java/com/laioffer/job/ExampleFiles/external/GitHubClient.java
@@ -0,0 +1,70 @@
+package com.laioffer.job.ExampleFiles.external;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class GitHubClient {
+    private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
+    private static final String DEAFAULT_KEYWORD = "developer";
+
+    //public String search(double lat, double lon, String keyword) {
+    public List<Item> search(double lat, double lon, String keyword) {
+        if (keyword == null) {
+            keyword = DEAFAULT_KEYWORD;
+        }
+
+        // "hello world" => "hello%20word"
+        // 把空格替换为%20
+        try {
+            keyword = URLEncoder.encode(keyword, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String url = String.format(URL_TEMPLATE, keyword, lat, lon);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //create a custom response handler
+        //ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+        ResponseHandler<List<Item>> responseHandler = new ResponseHandler<List<Item>>() {
+            @Override
+            public List<Item> handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    //return "";
+                    return Collections.emptyList();
+                }
+                HttpEntity entity = response.getEntity();
+                if (entity == null) {
+                    //return "";
+                    return Collections.emptyList();
+                }
+                //return EntityUtils.toString(entity);
+                ObjectMapper mapper = new ObjectMapper();
+                return Arrays.asList(mapper.readValue(entity.getContent(), Item[].class));
+            }
+        };
+
+        try {
+            return httpClient.execute(new HttpGet(url), responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //return "";
+        return Collections.emptyList();
+    }
+}
---
+++ src/main/java/com/laioffer/job/ExampleFiles/servlet/SearchServlet.java
@@ -0,0 +1,36 @@
+package com.laioffer.job.ExampleFiles.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
+import com.laioffer.job.external.GitHubClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
+public class SearchServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        GitHubClient client = new GitHubClient();
+
+        //model layer
+
+        response.setContentType("application/json");
+        //String itemsString = client.search(lat, lon, null);
+        //response.getWriter().print(itemsString);
+        List<Item> items = client.search(lat, lon, null);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getWriter(), items);
+    }
+}
